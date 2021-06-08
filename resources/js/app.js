6496408e9e07bdfc8ee3f83ca5f5de1d00c15050
alex <alenvolker2018@yandex.ua>
2021-06-08 10:44:50 +0300

    project ready

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,10 +1,13 @@
-require('./bootstrap');
 
-// Import modules...
+// Include modules...
+require('./bootstrap');
 import { createApp, h } from 'vue';
 import { App as InertiaApp, plugin as InertiaPlugin } from '@inertiajs/inertia-vue3';
 import { InertiaProgress } from '@inertiajs/progress';
+import { setLogLevel } from 'webpack/hot/log';
 
+
+// Inertia
 const el = document.getElementById('app');
 
 createApp({
@@ -19,3 +22,13 @@ createApp({
     .mount(el);
 
 InertiaProgress.init({ color: '#4B5563' });
+
+
+// Clear console
+console.clear();
+setLogLevel('none');
+
+console.info = function () {
+    if (arguments[0].indexOf('[webpack-dev-server]') == -1)
+        return console.info.apply(console, arguments);
+};
--- webpack.mix.js
+++ webpack.mix.js
@@ -10,11 +10,21 @@ const mix = require('laravel-mix');
  | file for the application as well as bundling up all the JS files.
  |
  */
+let production = mix.inProduction();
 
 mix.js('resources/js/app.js', 'public/js').vue()
     .sass('resources/sass/app.scss', 'public/css')
-    .webpackConfig(require('./webpack.config'));
+    .webpackConfig(require('./webpack.config'))
+    .sourceMaps(!production, 'source-map')
+    .disableNotifications()
+    .browserSync({
+        proxy: 'store-breeze2',
+        host: 'store-breeze2',
+        notify: false,
+        open: 'external'
+    });
 
-if (mix.inProduction()) {
+if (production) {
     mix.version();
 }
+
