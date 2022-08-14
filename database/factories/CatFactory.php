f51285febb3b12380159aefa20ca05e36a245d71
alex <alenvolker2018@yandex.ua>
2022-08-14 17:33:15 +0300

    add unsplash provider(faker)

--- database/factories/CatFactory.php
+++ database/factories/CatFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Services\Faker\Provider\Unsplash;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class CatFactory extends Factory
@@ -13,11 +14,13 @@ class CatFactory extends Factory
      */
     public function definition()
     {
-		$this->faker->addProvider(new \Xvladqt\Faker\LoremFlickrProvider($this->faker));
+
+		$this->faker->addProvider(new Unsplash($this->faker));
+
 
         return [
             'name' => $this->faker->firstName,
-			'image' => $this->faker->imageUrl(300, 300, ['mobile', 'device']),
+			'image' => $this->faker->imageUrl(300, 300, ['cat']),
 			'character' => $this->faker->randomElement(['good', 'bad', 'nice', 'angry']),
         ];
     }
